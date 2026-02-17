63eb6a7fd3d1b024b0706315605904c9293df06b
Vu Le <vusters@gmail.com>
2026-02-16 19:53:16 -0500

    uptake new widget version with fixed maps

--- apps/web/src/components/widgets/chat-widgets.tsx
+++ apps/web/src/components/widgets/chat-widgets.tsx
@@ -4,11 +4,6 @@ import { MapWidget, mapWidgetPropsSchema, MapWidgetProps } from "./MapWidget";
 
 export const customChatWidgets: UIWidgetDefinition[] = [
   {
-    widgetType: "vendor-card",
-    schema: vendorCardPropsSchema,
-    render: ({payload}) => <VendorCard {...payload as VendorCardProps} />,
-  },
-  {
     widgetType: "map-widget",
     schema: mapWidgetPropsSchema,
     render: ({payload}) => <MapWidget {...payload as MapWidgetProps} />,
