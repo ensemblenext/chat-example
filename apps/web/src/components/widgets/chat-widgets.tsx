6d3939d798c7751b5c0b7c39c7eede522bc4754d
Vu Le <vusters@gmail.com>
2026-02-18 22:54:56 -0500

    updated widget definitions

--- apps/web/src/components/widgets/chat-widgets.tsx
+++ apps/web/src/components/widgets/chat-widgets.tsx
@@ -1,11 +1,10 @@
 import { UIWidgetDefinition } from "@ensembleapp/client-sdk";
-import { VendorCard, vendorCardPropsSchema, VendorCardProps } from "./VendorCard";
 import { MapWidget, mapWidgetPropsSchema, MapWidgetProps } from "./MapWidget";
 
 export const customChatWidgets: UIWidgetDefinition[] = [
   {
     widgetType: "map-widget",
     schema: mapWidgetPropsSchema,
-    render: ({payload}) => <MapWidget {...payload as MapWidgetProps} />,
-  },
+    render: (payload) => <MapWidget {...payload as MapWidgetProps} />,
+  }
 ];
